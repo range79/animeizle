e425033250e31d49dc5b42eca8cf1bdcfac89b89
range79 <darkrange006@gmail.com>
2025-06-22 15:42:31 +0400

    feat EpisodeService added but not implemented feat: EpisodeRequest added

---
+++ src/main/kotlin/com/range/animeizle/animes/dto/request/EpisodeRequest.kt
@@ -0,0 +1,9 @@
+package com.range.animeizle.animes.dto.request
+
+data class EpisodeRequest (
+
+    val episodeNumber: Int,
+    val title: String,
+    val description: String,
+    val link: String,
+)
---
+++ src/main/kotlin/com/range/animeizle/animes/service/EpisodeService.kt
@@ -0,0 +1,20 @@
+package com.range.animeizle.animes.service
+
+import com.range.animeizle.animes.domain.enums.EpisodeStatus
+import com.range.animeizle.animes.dto.request.EpisodeRequest
+import com.range.animeizle.animes.dto.response.EpisodeResponse
+
+interface EpisodeService {
+
+    fun findAllSeasonEpisodes(): List<EpisodeResponse>
+    fun setEpisodeStatus(id: Long, status: EpisodeStatus): EpisodeResponse
+    fun updateEpisode(seasonId: Long, episodeRequest: EpisodeRequest): EpisodeResponse
+    fun deleteEpisode(id: Long,description: Boolean): EpisodeResponse
+    fun findAll(): List<EpisodeResponse>
+
+
+
+
+
+
+}
