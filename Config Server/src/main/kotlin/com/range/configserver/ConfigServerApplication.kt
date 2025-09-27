48bf66781bcad7f47503a4d1292e865c99ecde9a
range79 <darkrange006@gmail.com>
2025-09-27 12:46:55 +0400

    feat: Config server and Eureka server added

---
+++ Config Server/src/main/kotlin/com/range/configserver/ConfigServerApplication.kt
@@ -0,0 +1,11 @@
+package com.range.configserver
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ConfigServerApplication
+
+fun main(args: Array<String>) {
+    runApplication<ConfigServerApplication>(*args)
+}
---
+++ EurekaServer/src/test/kotlin/com/range/eurukaserver/EurukaServerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.range.eurukaserver
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class EurukaServerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
