224b0c9ee1527f16131a3c97a294bb661a4d4dcb
range79 <darkrange006@gmail.com>
2025-12-02 04:06:15 +0400

    photo service added

--- src/main/kotlin/com/range/rangeWatch/common/service/AmazonService.kt
+++ src/main/kotlin/com/range/rangeWatch/common/service/AmazonService.kt
@@ -1,7 +1,4 @@
 package com.range.rangeWatch.common.service
-import org.springframework.web.multipart.MultipartFile
 
-interface AmazonService {
-    fun addPhoto(bucketName: String, file: MultipartFile, fileName: String):String
-    fun deletePhoto(bucketName: String, filename: String?)
+class AmazonService {
 }
